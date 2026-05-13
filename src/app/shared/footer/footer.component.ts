89c38ec720f0f110ad2b4d0fd9260c4dc18c8a19
hponepyae <hponepyaeee@gmail.com>
2026-05-13 16:11:50 +0630

    feat: publish new branch

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  templateUrl: './footer.component.html',
+  templateUrl: './footer.component.html', 
 })
 export class FooterComponent {
   protected readonly year = new Date().getFullYear();
